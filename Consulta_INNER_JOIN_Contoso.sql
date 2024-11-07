38ae13a28210c0af51e5d445b34e60cc0b6eecce
Nathalia <156125029+nath9alves@users.noreply.github.com>
2024-11-07 14:44:52 -0300

    Update Consulta_INNER_JOIN_Contoso.sql

--- Consulta_INNER_JOIN_Contoso.sql
+++ Consulta_INNER_JOIN_Contoso.sql
@@ -1,11 +1,11 @@
---SELECT productkey,
---productname,
---DimProductSubcategory.ProductSubcategoryKey,
---productsubcategoryname,
---DimProductSubcategory.ProductCategoryKey,
---DimProductCategory.ProductCategoryName
---FROM DimProduct
---INNER JOIN DimProductSubcategory
---ON DimProduct.ProductSubcategoryKey = DimProductSubcategory.ProductSubcategoryKey
---INNER JOIN DimProductCategory
---ON DimProductSubcategory.ProductCategoryKey = DimProductCategory.ProductCategoryKey ------ > Consulta com INNER JOIN 
+SELECT productkey,
+productname,
+DimProductSubcategory.ProductSubcategoryKey,
+productsubcategoryname,
+DimProductSubcategory.ProductCategoryKey,
+DimProductCategory.ProductCategoryName
+FROM DimProduct
+INNER JOIN DimProductSubcategory
+ON DimProduct.ProductSubcategoryKey = DimProductSubcategory.ProductSubcategoryKey
+INNER JOIN DimProductCategory
+ON DimProductSubcategory.ProductCategoryKey = DimProductCategory.ProductCategoryKey ------ > Consulta com INNER JOIN 
